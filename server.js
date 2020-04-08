046395b0a217ebf3a7d2269c5de604ef2fa5e581
Jeenil Patel <admin@admins-MacBook-Pro-2.local>
2020-04-08 15:02:19 -0400

    connected to the server

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var morgan = require('morgan');
+const MongoClient = require('mongodb').MongoClient;
 
-const mongo = require('mongodb');
-
-var mongURL = 'mongodb+srv://Jeenil:1234@cs326finalproject-xfsi3.mongodb.net/test?retryWrites=true&w=majority';
-
-const clinet = new mongo.MongoClient(mongURL);
-
-const databaseName = 'cs326';
-
-const collectionName = 'Pokemon';
-
-clinet.connect( function(err, db) {
-    var db = mongoclient.db(databaseName);
-    var pokemonCollection = db.collection(collectionName)
-    collection.find()
-
-    
-
-    });
-
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://Jeenil:1234@cs326finalproject-xfsi3.mongodb.net/test?retryWrites=true&w=majority'"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
