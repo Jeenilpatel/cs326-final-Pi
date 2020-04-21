492f6bc9cd1a17f74f60fd0cd2998b2ebc2443e6
jeenilpatel <jeenilpatel@umass.edu>
2020-04-21 19:57:59 -0400

    Teams Page

--- html/poke_Assist.js
+++ html/poke_Assist.js
@@ -294,7 +294,7 @@ function fetchPokemon(PokeID, pokemon_Num, pokemon_Name) {
     );
 };
 
-function team_Update(){
+function HTML_Image_Update(){
     fetchPokemon('PokeID1', 'pokemon1', 'pokemon1_Name');
     fetchPokemon('PokeID2', 'pokemon2', 'pokemon2_Name');
     fetchPokemon('PokeID3', 'pokemon3', 'pokemon3_Name');
@@ -339,3 +339,5 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
